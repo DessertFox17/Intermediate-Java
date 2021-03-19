6de9e9b47f3d3c79ffbb108eae48ecea6b644ebf
Juan Arango <jblackheart581@gmail.com>
2021-03-19 03:39:40 -0500

    Resolving the mistake mentioned before

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -2,6 +2,7 @@ package com.main;
 
 import java.util.Scanner;
 
+import lavadoras.LavadoraUno;
 import poobj.POOmain;
 import usothis.ARmain;
 
@@ -9,14 +10,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Scanner teclado1 = new Scanner(System.in);
         Scanner teclado2 = new Scanner(System.in);
         String resp = "Si";
         int caso = 0;
 
         do {
-
             System.out.println("");
             System.out.println("--------------------------------------------------");
             System.out.println("Prácticas Curso de programación en java Intermedio");
@@ -29,66 +28,87 @@ public class Main {
             System.out.println("3.Comentarios");
             System.out.println("4.POO+");
             System.out.println("5.Uso de la palabra reservada this");
+            System.out.println("6.Uso del encapsulamiento");
             System.out.print("-> ");
             caso = teclado1.nextInt();
 
             switch (caso) {
 
                 case 1:
-
                     System.out.println("");
                     System.out.println("Probando tu lógica de programación");
                     System.out.println("");
                     ProbandoLogica A = new ProbandoLogica();
                     A.Xl();
+<<<<<<< HEAD
 
+=======
+>>>>>>> be51987 (NewVlog about encapsulation, classes of encapsulation were created and added to the main switch-case structure, one mistake was notified, I was working on the master branch, this is pendiente to be corrected)
                     break;
 
                 case 2:
-
                     System.out.println("");
                     System.out.println("Palabras reservadas en Java");
                     System.out.println("");
                     PalabrasReservadas B = new PalabrasReservadas();
                     B.Palabras();
+<<<<<<< HEAD
 
+=======
+>>>>>>> be51987 (NewVlog about encapsulation, classes of encapsulation were created and added to the main switch-case structure, one mistake was notified, I was working on the master branch, this is pendiente to be corrected)
                     break;
 
                 case 3:
-
                     System.out.println("");
                     System.out.println("Cometarios");
                     System.out.println("");
                     Comentarios C = new Comentarios();
                     C.Comments();
+<<<<<<< HEAD
 
+=======
+>>>>>>> be51987 (NewVlog about encapsulation, classes of encapsulation were created and added to the main switch-case structure, one mistake was notified, I was working on the master branch, this is pendiente to be corrected)
                     break;
 
                 case 4:
-
                     System.out.println("");
                     System.out.println("POO+");
                     System.out.println("");
                     POOmain D = new POOmain();
                     D.POOm();
+<<<<<<< HEAD
 
+=======
+>>>>>>> be51987 (NewVlog about encapsulation, classes of encapsulation were created and added to the main switch-case structure, one mistake was notified, I was working on the master branch, this is pendiente to be corrected)
                     break;
 
                 case 5:
-
                     System.out.println("");
                     System.out.println("Uso de la palabra reservada this");
                     System.out.println("");
                     ARmain E = new ARmain();
                     E.armaina();
+<<<<<<< HEAD
 
+=======
+                    break;
+
+                case 6:
+                    System.out.println("");
+                    System.out.println("Uso del encapsulamiento");
+                    System.out.println("");
+                    LavadoraUno F = new LavadoraUno();
+                    F.LavadoraDatos();
+>>>>>>> be51987 (NewVlog about encapsulation, classes of encapsulation were created and added to the main switch-case structure, one mistake was notified, I was working on the master branch, this is pendiente to be corrected)
                     break;
 
                 default:
-
                     System.out.println("");
                     System.out.println("Opción inválida");
+<<<<<<< HEAD
 
+=======
+>>>>>>> be51987 (NewVlog about encapsulation, classes of encapsulation were created and added to the main switch-case structure, one mistake was notified, I was working on the master branch, this is pendiente to be corrected)
                     break;
 
             }
